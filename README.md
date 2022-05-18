1d47aa86674f4bd14a1a34109904f397042147f5
reem-elbakry <reemmousa1995@gmail.com>
2022-05-18 06:08:27 +0200

    detect the winner

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 1. import the used font in index.js (google font >> fredoka)
 2. 3 main components 
-`- box component
+    - box component in board component
+3. there are 8 win conditions for each player 
+    - 0,1,2 - 3,4,5 - 6,7,8 - 0,3,6 - 1,4,7 - 2,5,8 - 0,4,8 - 2,4,6 
+    - need a function to check the board every time when the player plays (won or not!) 
--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Board from './components/Board/Board';
 import React, { useState } from 'react';
 
 function App() {
+  const WIN_CONDITIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6], 
+];
   const [ board, setBoard] = useState(Array(9).fill(null));
   const [ xPlaying, setXPlaying ] = useState(true); //x will start the game always
   const handleBoxClick = (boxIndex)=>{
@@ -13,8 +23,18 @@ function App() {
         return value
       }
     })
+    console.log(checkWinner(updatedBox));
     setBoard(updatedBox)
     setXPlaying(!xPlaying)
+  }
+  const checkWinner = (board)=>{
+    for(var i=0; i<WIN_CONDITIONS.length; i++){
+      const [x, y, z] = WIN_CONDITIONS[i]
+      
+      if(board[x] && board[x] === board[y] && board[y] === board[z]){
+        return board[x]
+      }
+    }
   }
   return (
     <>
